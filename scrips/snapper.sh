d605643a8f09f4041006ac42b8383b3566523dcd
Fin392 <fin392@gmail.com>
2026-07-08 20:07:34 +0200

    Create snapper.sh

---
+++ scrips/snapper.sh
@@ -0,0 +1,11 @@
+# Instalación de Snapper
+
+# Desinstalar
+sudo apt purge firefox* -y
+sudo rm --recursive /usr/lib/firefox-esr
+sudo rm --recursive /usr/share/firefox-esr
+rm --recursive ~/.mozilla/firefox
+rm --recursive ~/.cache/mozilla/firefox
+
+# Instalar
+sudo apt install firefox-esr-l10n-es-es -y
