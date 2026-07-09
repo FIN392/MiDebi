a88fdeb88490e68ef092c4d7ef72295e9615072c
Fin392 <fin392@gmail.com>
2026-07-09 20:14:36 +0200

    Add installation and uninstallation comments for Snapper
    Added commented-out commands for uninstalling and installing Snapper.

--- scripts/snapper.sh
+++ scripts/snapper.sh
@@ -4,4 +4,15 @@ trap 'rc=$?; echo "ERROR: \"$BASH_COMMAND\" falló en la línea $LINENO (código
 
 # Instalación de Snapper
 
+# Desinstalar
+# sudo apt purge ...* -y
+# sudo rm --force --recursive /usr/lib/firefox-esr
+# sudo rm --force --recursive /usr/share/firefox-esr
+# rm --force --recursive ~/.mozilla/firefox
+# rm --force --recursive ~/.cache/mozilla/firefox
+
+# Instalar
+# sudo apt install ... y
+
+# Configurar
 # ...
