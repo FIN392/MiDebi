175a2ed87a6b24c35be377475738af4c07a84efa
Fin392 <fin392@gmail.com>
2026-07-09 20:08:58 +0200

    Update Firefox uninstallation commands to use --force
    Changed removal commands to use --force option for safer execution.

--- scripts/firefox.sh
+++ scripts/firefox.sh
@@ -6,10 +6,10 @@ trap 'rc=$?; echo "ERROR: \"$BASH_COMMAND\" falló en la línea $LINENO (código
 
 # Desinstalar
 sudo apt purge firefox* -y
-sudo rm --recursive /usr/lib/firefox-esr
-sudo rm --recursive /usr/share/firefox-esr
-rm --recursive ~/.mozilla/firefox
-rm --recursive ~/.cache/mozilla/firefox
+sudo rm --force --recursive /usr/lib/firefox-esr
+sudo rm --force --recursive /usr/share/firefox-esr
+rm --force --recursive ~/.mozilla/firefox
+rm --force --recursive ~/.cache/mozilla/firefox
 
 # Instalar
 sudo apt install firefox-esr-l10n-es-es -y
