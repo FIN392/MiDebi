2eb66046556987318765198de334a1d826274943
Fin392 <fin392@gmail.com>
2026-07-09 20:30:15 +0200

    Update snapper.sh

--- scripts/snapper.sh
+++ scripts/snapper.sh
@@ -4,15 +4,25 @@ trap 'rc=$?; echo "ERROR: \"$BASH_COMMAND\" falló en la línea $LINENO (código
 
 # Instalación de Snapper
 
-# Desinstalar
-# sudo apt purge ...* -y
-# sudo rm --force --recursive /usr/lib/firefox-esr
-# sudo rm --force --recursive /usr/share/firefox-esr
-# rm --force --recursive ~/.mozilla/firefox
-# rm --force --recursive ~/.cache/mozilla/firefox
-
 # Instalar
-# sudo apt install ... y
+sudo apt install snapper -y
 
 # Configurar
-# ...
+# Crear la configuración de Snapper para root
+sudo snapper -c root create-config /
+
+# Ajustar la configuración (límites y timeline)
+snapper -c root set-config \
+    TIMELINE_CREATE="yes" \
+    TIMELINE_CLEANUP="yes" \
+    TIMELINE_LIMIT_HOURLY="24" \
+    TIMELINE_LIMIT_DAILY="7" \
+    TIMELINE_LIMIT_WEEKLY="4" \
+    TIMELINE_LIMIT_MONTHLY="12" \
+    TIMELINE_LIMIT_YEARLY="1" \
+    NUMBER_CLEANUP="yes" \
+    NUMBER_LIMIT="10" \
+    NUMBER_LIMIT_IMPORTANT="10"
+
+# Crear un snapshot manual de prueba
+sudo snapper -c root create --description "Snapshot inicial"
