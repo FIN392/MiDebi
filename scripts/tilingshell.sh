5a26b3b17d8ab7046ff269a97188d28dd869cdef
Fin392 <fin392@gmail.com>
2026-07-15 21:41:52 +0200

    Update tilingshell.sh

--- scripts/tilingshell.sh
+++ scripts/tilingshell.sh
@@ -5,23 +5,10 @@ trap 'rc=$?; echo "ERROR: \"$BASH_COMMAND\" falló en la línea $LINENO (código
 # Instalación de Tiling Shell
 
 # Instalar
-EXTENSION_ID="tilingshell@ferrarodomenico.com"
-INSTALL_DIR="$HOME/.local/share/gnome-shell/extensions/$EXTENSION_ID"
-curl -L -o /tmp/tilingshell.zip "https://extensions.gnome.org/download-extension/${EXTENSION_ID}.shell-extension.zip?version_tag=70233"
-mkdir -p "$INSTALL_DIR"
-tar -xf /tmp/tilingshell.zip -C "$INSTALL_DIR"
+curl -L -o /tmp/tilingshell.zip "https://extensions.gnome.org/download-extension/tilingshell@ferrarodomenico.com.shell-extension.zip?version_tag=70233"
+gnome-extensions install --force /tmp/tilingshell.zip
+gnome-extensions enable tilingshell@ferrarodomenico.com
 rm --force /tmp/tilingshell.zip
-CURRENT_EXTENSIONS=$(gsettings get org.gnome.shell enabled-extensions)
-if [[ "$CURRENT_EXTENSIONS" == *"$EXTENSION_ID"* ]]; then
-    echo "La extensión ya estaba habilitada en el sistema."
-else
-    if [ "$CURRENT_EXTENSIONS" = "@as []" ] || [ "$CURRENT_EXTENSIONS" = "[]" ] || [ -z "$CURRENT_EXTENSIONS" ]; then
-        NEW_EXTENSIONS="['$EXTENSION_ID']"
-    else
-        NEW_EXTENSIONS="${CURRENT_EXTENSIONS%]*}, '$EXTENSION_ID']"
-    fi
-    gsettings set org.gnome.shell enabled-extensions "$NEW_EXTENSIONS"
-fi
 
 # Configurar
 SCHEMA_DIR="$HOME/.local/share/gnome-shell/extensions/tilingshell@ferrarodomenico.com/schemas"
