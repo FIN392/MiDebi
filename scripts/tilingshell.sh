57ea56ccde88feaf63a6da90620c23bb8413a36d
Fin392 <fin392@gmail.com>
2026-07-15 19:27:56 +0200

    Add installation commands for Tiling Shell extension
    Added commands to install and enable Tiling Shell extension.

--- scripts/tilingshell.sh
+++ scripts/tilingshell.sh
@@ -5,3 +5,9 @@ trap 'rc=$?; echo "ERROR: \"$BASH_COMMAND\" falló en la línea $LINENO (código
 # Instalación de Tiling Shell
 
 # Instalar
+curl -L -o /tmp/tilingshell.zip "https://extensions.gnome.org/download-extension/tilingshell@ferrarodomenico.com.shell-extension.zip?version_tag=70233"
+gnome-extensions install --force /tmp/tilingshell.zip
+gnome-extensions enable tilingshell@ferrarodomenico.com
+rm --force /tmp/tilingshell.zip
+
+#
