f7569aaa5bb75fcdf52aab8d47ed520eae9b8cb2
Fin392 <fin392@gmail.com>
2026-07-15 19:25:55 +0200

    Update tilingshell.sh

--- scripts/tilingshell.sh
+++ scripts/tilingshell.sh
@@ -1,1 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -Eeuo pipefail
+trap 'rc=$?; echo "ERROR: \"$BASH_COMMAND\" falló en la línea $LINENO (código de salida: $rc)" >&2; exit "$rc"' ERR
+
+# Instalación de Tiling Shell
+
+# Instalar
