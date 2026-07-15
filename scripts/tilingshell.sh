ee35c559e8e53b3b5bc91c4cce122a9abe61cd62
Fin392 <fin392@gmail.com>
2026-07-15 19:29:02 +0200

    Configure layouts for tilingshell extension
    Add layout configurations for tilingshell extension

--- scripts/tilingshell.sh
+++ scripts/tilingshell.sh
@@ -10,4 +10,11 @@ gnome-extensions install --force /tmp/tilingshell.zip
 gnome-extensions enable tilingshell@ferrarodomenico.com
 rm --force /tmp/tilingshell.zip
 
-#
+# Configurar
+SCHEMA_DIR="$HOME/.local/share/gnome-shell/extensions/tilingshell@ferrarodomenico.com/schemas"
+LAYOUTS_JSON='[
+  {"id": "2 Columnas (50/50)", "tiles": [{"x": 0, "y": 0, "width": 0.5, "height": 1}, {"x": 0.5, "y": 0, "width": 0.5, "height": 1}]},
+  {"id": "2 Filas (50/50)", "tiles": [{"x": 0, "y": 0, "width": 1, "height": 0.5}, {"x": 0, "y": 0.5, "width": 1, "height": 0.5}]},
+  {"id": "4 Esquinas (2x2)", "tiles": [{"x": 0, "y": 0, "width": 0.5, "height": 0.5}, {"x": 0.5, "y": 0, "width": 0.5, "height": 0.5}, {"x": 0, "y": 0.5, "width": 0.5, "height": 0.5}, {"x": 0.5, "y": 0.5, "width": 0.5, "height": 0.5}]}
+]'
+gsettings --schemadir "$SCHEMA_DIR" set org.gnome.shell.extensions.tilingshell layouts-json "$LAYOUTS_JSON"
