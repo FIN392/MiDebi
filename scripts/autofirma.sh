91dcd281956f6ab715ea09ceeb53b7a182dae5ed
Fin392 <fin392@gmail.com>
2026-07-20 22:53:07 +0200

    Update autofirma.sh

--- scripts/autofirma.sh
+++ scripts/autofirma.sh
@@ -5,20 +5,30 @@ trap 'rc=$?; echo "ERROR: \"$BASH_COMMAND\" falló en la línea $LINENO (código
 # Instalación de Autofirma (Gobierno de España)
 
 # Desinstalar
-sudo apt purge firefox* -y
-# sudo rm --force --recursive /usr/lib/firefox-esr
-# sudo rm --force --recursive /usr/share/firefox-esr
-# rm --force --recursive ~/.mozilla/firefox
-# rm --force --recursive ~/.cache/mozilla/firefox
+sudo apt purge default-jre -y
+sudo apt purge autofirma* -y
+sudo apt autoremove -y
 
 # Instalar
+sudo apt install default-jre -y
 curl -L -o /tmp/autofirma.zip "https://firmaelectronica.gob.es/content/dam/firmaelectronica/descargas-software/autofirma19/Autofirma_Linux_Debian.zip"
+python3 -m zipfile -e /tmp/autofirma.zip /tmp/
+sudo apt install ./autofirma_1_9.deb -y
 
-
-
-
-# sudo apt install firefox-esr-l10n-es-es -y
-
+# Instalar certificados y despues...
+# sudo update-ca-certificates
 
 # Configurar
+cat << 'EOF' > /tmp/autofirma_config.afconfig
+<?xml version="1.0" encoding="UTF-8"?>
+<plist version="1.0">
+    <dict>
+        <key>padesVisibleStamp</key><true/>
+        <key>default.locale</key><string>es_ES</string>
+        <key>padesVisibleSignature</key><true/>
+    </dict>
+</plist>
+EOF
 
+# Herremientas / Preferencias / Importar configuración
+#     /tmp/autofirma_config.afconfig
