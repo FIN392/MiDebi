09cb8e036b3e44098a6643f44556468afa69911e
Fin392 <fin392@gmail.com>
2026-08-12 16:58:53 +0200

    Update fastfetch.sh

--- software/fastfetch.sh
+++ software/fastfetch.sh
@@ -2,15 +2,15 @@
 set -Eeuo pipefail
 trap 'rc=$?; echo "ERROR: \"$BASH_COMMAND\" falló en la línea $LINENO (código de salida: $rc)" >&2; exit "$rc"' ERR
 
-# Instalación de ......
+# Instalación de fastfetch
 
 # Desinstalar
-sudo apt purge ...... -y
+sudo apt purge "fastfetch*" -y || true
 sudo apt autoremove -y
+rm -R ~/.config/fastfetch/
 
 # Instalar
-sudo apt install ...... -y
+sudo apt install fastfetch -y
 
 # Configurar
-# ......
-
+fastfetch --gen-config
