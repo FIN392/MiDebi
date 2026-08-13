e20d08e3a65696962cc017c9eca254f491fc2969
Fin392 <fin392@gmail.com>
2026-08-13 20:45:32 +0200

    Update brave.sh

--- software/brave.sh
+++ software/brave.sh
@@ -3,6 +3,7 @@ set -Eeuo pipefail
 trap 'rc=$?; echo "ERROR: \"$BASH_COMMAND\" falló en la línea $LINENO (código de salida: $rc)" >&2; exit "$rc"' ERR
 
 # Instalación de Brave Origin
+DIR_ACTUAL="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Desinstalar
 sudo apt purge "brave*" -y || true
@@ -14,6 +15,4 @@ curl -fsS https://dl.brave.com/install.sh | FLAVOR=origin sh
 
 # Configurar
 # tar -czvf "$HOME/BraveSoftware.tar.gz" -C "$HOME/.config" BraveSoftware
-
-# tar -xzvf "$HOME/BraveSoftware.tar.gz" -C "$HOME/.config"
-
+tar -xzvf "$DIR_ACTUAL/BraveSoftware.tar.gz" -C "$HOME/.config"
