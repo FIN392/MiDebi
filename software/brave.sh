649fdc79eab8e21f252887bd93670c9b4678197e
Fin392 <fin392@gmail.com>
2026-08-13 20:18:42 +0200

    Update brave.sh

--- software/brave.sh
+++ software/brave.sh
@@ -7,7 +7,7 @@ trap 'rc=$?; echo "ERROR: \"$BASH_COMMAND\" falló en la línea $LINENO (código
 # Desinstalar
 sudo apt purge "brave*" -y || true
 sudo apt autoremove -y
-rm -R ~/.config/BraveSoftware/
+rm -rf ~/.config/BraveSoftware/
 
 # Instalar
 curl -fsS https://dl.brave.com/install.sh | FLAVOR=origin sh
