c635b01006b64196f5872290890cf14729bf9399
Fin392 <fin392@gmail.com>
2026-08-20 18:12:18 +0200

    Update firefox.sh

--- config/firefox.sh
+++ config/firefox.sh
@@ -134,10 +134,3 @@ sudo tee "/usr/lib/firefox-esr/distribution/policies.json" > /dev/null << 'EOF'
   }
 }
 EOF
-
-# Primer inicio: abre y cierra
-firefox
-sleep 5s
-sudo pkill -TERM firefox || true
-sleep 5s
-sudo pkill -KILL firefox || true
